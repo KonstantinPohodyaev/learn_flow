07e491e60768e636571d0a54612498dff96806ed
kspohodyaev <konstantinpohodyaev@yandex.ru>
2025-08-10 23:12:22 -0700

    Add registration CBV and template

--- learn_flow/learn_flow/urls.py
+++ learn_flow/learn_flow/urls.py
@@ -1,11 +1,23 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.views.generic import CreateView
+from django.urls import include, path, reverse_lazy
+
+from users.forms import CustomUserCreationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quiz/', include('quizzes.urls')),
+    path(
+        'auth/registration',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=CustomUserCreationForm,
+            success_url=reverse_lazy('login')
+        ),
+        name='registration'
+    ),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('courses.urls'))
 ]
