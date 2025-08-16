a0bccceff9211f5725dbdb05bf3be1a14cc054d7
kspohodyaev <konstantinpohodyaev@yandex.ru>
2025-08-15 23:17:18 -0700

    Add admin tools for Course model

--- learn_flow/courses/admin.py
+++ learn_flow/courses/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
-
+from django.urls import reverse
+from django.utils.html import format_html_join
 from .models import Certificate, Course, CourseProgress, Lesson, Module
 
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'description',
+        'owner',
+        'modules_field'
+    ]
+    search_fields = [
+        'title', 'owner__email'
+    ]
+    list_filter = [
+        'title', 'owner__email'
+    ]
+    list_display_links = [
+        'title'
+    ]
+
+
+    @admin.display(
+        description='Модули'
+    )
+    def modules_field(self, course):
+        return format_html_join(
+            '\n',
+            '<a href="{}">{}</a>',
+            (
+                (reverse(
+                    'admin:courses_module_change',
+                    args=[module.id]
+                ), module.title)
+                for module in course.modules.all()
+            )
+        )
+
+
 admin.site.register(Module)
 admin.site.register(Lesson)
 admin.site.register(CourseProgress)
